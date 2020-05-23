fd3c9cc4efd6149349504d6648e6e910b4683a98
Cefo14 <cefo_edd@hotmail.com>
2020-05-23 09:43:01 +0000

    Deploying to gh-pages from  @ b937cb9a20011d63ea4cfef756276a04e7291e9c 🚀

---
+++ precache-manifest.38528261b5c93a6249dfacdccb84f414.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee550c12154ba3069377a147c6af287",
+    "url": "/index.html"
+  },
+  {
+    "revision": "624c2bc34432a92d0b0a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b43e637cd8cb79765119",
+    "url": "/static/js/2.d3e77141.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d3e77141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d32578e730818c4ea4fa",
+    "url": "/static/js/3.c35ef404.chunk.js"
+  },
+  {
+    "revision": "61e587a167ff409f6635",
+    "url": "/static/js/4.a84125f0.chunk.js"
+  },
+  {
+    "revision": "d88d66b5657745b8fd54",
+    "url": "/static/js/5.00a67f47.chunk.js"
+  },
+  {
+    "revision": "253ce0ad30b783b1ac9a",
+    "url": "/static/js/6.0bc35740.chunk.js"
+  },
+  {
+    "revision": "624c2bc34432a92d0b0a",
+    "url": "/static/js/main.3b845bd8.chunk.js"
+  },
+  {
+    "revision": "d9e2763b6d6a62c4ab6e",
+    "url": "/static/js/runtime-main.415b011a.js"
+  },
+  {
+    "revision": "437e9f4c671f4feddf16155d6f376667",
+    "url": "/static/media/bg.437e9f4c.webp"
+  },
+  {
+    "revision": "4caba81b6da6ed998be508a31bfa89c2",
+    "url": "/static/media/logo.4caba81b.webp"
+  }
+]);
